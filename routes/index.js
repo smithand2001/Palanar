03735d7bdafc25a1400f12cf01fe60ac9dd2c2ab
Andrew Smith <52361650+smithand2001@users.noreply.github.com>
2023-04-18 18:38:42 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const User = require('../models/User');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,9 +7,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/login', function(req, res, next) {
-  console.log(req.body.username+" - "+req.body.password);
-  res.redirect("/home")
+router.post('/login', async function(req, res, next) {
+  const user = await User.findUser(req.body.username, req.body.password)
+  if(user!== null){
+    res.redirect("/home")
+  }else{
+    res.redirect("/?msg=fail")
+  }
+  
 });
 
 module.exports = router;
