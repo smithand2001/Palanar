8b78e13fbc8f58d67968bc0216efb87cfbb3e09a
Andrew Smith <52361650+smithand2001@users.noreply.github.com>
2023-04-21 20:34:22 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,38 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database/cmsdb.sqlite');
+const { Sequelize } = require('sequelize')
 
+const sequelize = new Sequelize({
+  dialect: 'sqlite', 
+  storage: './database/palanar.sqlite'
+})
 
-let sql = `SELECT * FROM USER WHERE username=? and password=?` ;
-
-db.get(sql, ['subu','1234'], (err, row) => {
-  if (err) {
-    throw err;
-  }else{
-    console.log(row)
-  }
-});
-
-function getRow(sql, values) {
-    return new Promise((resolve, reject) =>{
-        db.get(sql,values, (err,row) => {
-            if(err) {
-                reject(err);
-            }else{
-                resolve(row);
-            }
-            });
-
-        })
-}
-
-async function getRowTest() {
-    let row = await getRow(sql,['subu', '1234'])
-    console.log(row)
-}
-
-getRowTest().then(()=>console.log('query completed'))
-
-module.exports = { getRow}
-
-// close the database connection
-db.close();
+module.exports = sequelize
