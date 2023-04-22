b708110d751a84a97d5555bcbd8c1a0bfe35b9c1
Andrew Smith <52361650+smithand2001@users.noreply.github.com>
2023-04-22 01:45:26 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,23 @@
-const { Sequelize } = require('sequelize')
+const sqlite3 = require('sqlite3').verbose();
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite', 
-  storage: './database/palanar.sqlite'
-})
+const db = new sqlite3.Database('./database/palanar1.sqlite', (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Connected to the palanar database.');
+});
 
-module.exports = sequelize
+const createUserTable = `CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL
+);`;
+
+db.run(createUserTable, (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Created the users table.');
+});
+
+module.exports = db;
