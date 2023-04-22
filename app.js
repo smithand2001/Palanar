5525b81655490ecba7101bcbdfe22ecac9a61b7e
Andrew Smith <52361650+smithand2001@users.noreply.github.com>
2023-04-22 01:45:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,25 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const session = require('express-session')
-const sequelize = require('./db')
-const User = require('./models/User')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index');
+const signupRouter = require('./routes/signup');
+const loginRouter = require('./routes/login');
 
-const sqlite3 = require('sqlite3').verbose();
+const app = express();
 
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
+// Set the view engine to EJS
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Set up body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'palanar489',
-  resave: false,
-  saveUinitialized: true,
-  cookie: { secure: false}
-}))
-
+// Use the index, signup, and login routers
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-
-
-async function setup() {
-  const user = await User.create({username: "tom", password: "123"});
-  console.log(`Created user ${user.username}`);
-
-  const user1 = await User.create({username: "joe", password: "123"});
-  console.log(`Created user ${user1.username}`);
-
-  
-}
-
-sequelize.sync({ force: true }).then(()=>{
-  console.log("Sequelize Sync Completed...")
-  setup().then(()=> console.log("User setup complete"))
-})
-
-
+app.use('/signup', signupRouter);
+app.use('/login', loginRouter);
 
 module.exports = app;
