20254c9d60e33de215f22057696d9ac8646c48c5
Andrew Smith <52361650+smithand2001@users.noreply.github.com>
2023-04-22 01:47:40 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,78 @@
-var express = require('express');
-const User = require('../models/User');
-var router = express.Router();
-const db = require('../models/User');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/palanar1.sqlite');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/login', (req, res) => {
-  res.render('login.ejs');
-});
-
-router.post('/login', async function(req, res, next) {
-  const user = await User.findUser(req.body.username, req.body.password)
-  if(user!== null){
-    req.session.user = user
-    res.redirect("/home")
-  }else{
-    res.redirect("/?msg=fail")
-  }
-  
+router.get('/', (req, res) => {
+  res.send('Welcome to Palanar!');
 });
 
 
-router.get('/signup', function(req, res, next) {
-  res.render('signup.ejs', { title: 'signup'});
+// Signup route
+router.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  // Hash the password
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) throw err;
+
+    // Insert the user into the database
+    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    db.run(sql, [username, hash], function(err) {
+      if (err) throw err;
+
+      console.log(`User ${username} has been registered`);
+      res.redirect('/login');
+    });
+  });
 });
 
 
-router.post('/signup', async function(req, res, next){
-  try{
-    await User.create(
-      {
-        username: req.body.username,
-        password: req.body.password
+// Login route
+router.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  // Hash the password
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) throw err;
+
+    // Insert the user into the database
+    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    db.run(sql, [username, hash], function(err) {
+      if (err) throw err;
+
+      console.log(`User ${username} has been registered`);
+      res.redirect('/login');
+    });
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if the username exists in the database
+  const sql = 'SELECT * FROM users WHERE username = ?';
+  db.get(sql, [username], (err, row) => {
+    if (err) throw err;
+
+    if (!row) {
+      return res.status(401).send('Invalid username or password');
+    }
+
+    // Check if the password is correct
+    bcrypt.compare(password, row.password, (err, result) => {
+      if (err) throw err;
+
+      if (!result) {
+        return res.status(401).send('Invalid username or password');
       }
-    )
-  
-  console.log({username, password});
-  
-  res.redirect('/login?msg=success')
-  } catch (error) {
-  res.redirect('/signup?msg=fail')
-  }
-  
+
+      console.log(`User ${row.username} has logged in`);
+      res.redirect('/home');
+    });
+  });
 });
-  
-
-
 
 
 module.exports = router;
